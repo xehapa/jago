f49f0124289b8723a295b3b99b6325989096669f
Xehapa <xehapa.dev@gmail.com>
2023-06-10 13:05:33 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/xehap/jago/config"
+)
+
 func main() {
-	println("Hello, World!")
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Enter client ID: ")
+	clientID, _ := reader.ReadString('\n')
+
+	fmt.Print("Enter client secret: ")
+	clientSecret, _ := reader.ReadString('\n')
+
+	cfg := config.NewConfig(clientID, clientSecret)
+
+	log.Print("Your Client ID is:" + cfg.APIKey + " and your Client Secret is " + cfg.APISecret)
 }
