bb1b655636005d7a4877312136e34844449e7a4b
Xehapa <xehapa.dev@gmail.com>
2023-06-10 12:58:54 +0700

    Update jobadder.go

--- api/jobadder.go
+++ api/jobadder.go
@@ -1,11 +1,6 @@
 package api
 
 import (
-	"encoding/json"
-	"io/ioutil"
-	"net/http"
-
-	"github.com/xehap/jago/models"
 	"github.com/xehap/jago/utils"
 )
 
@@ -21,38 +16,4 @@ func NewJobadderClient(apiKey, apiSecret string) *JobAdderClient {
 		apiSecret:  apiSecret,
 		httpClient: utils.NewHTTPClient(),
 	}
-}
-
-func (c *JobAdderClient) GetJobs() ([]models.Job, error) {
-	url := "https://api.jobadder.com/v2/jobs"
-	req, err := http.NewRequest("GET", url, nil)
-
-	if err != nil {
-		return nil, err
-	}
-
-	req.SetBasicAuth(c.apiKey, c.apiSecret)
-
-	resp, err := c.httpClient.Do(req)
-
-	if err != nil {
-		return nil, err
-	}
-
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-
-	if err != nil {
-		return nil, err
-	}
-
-	var jobs []models.Job
-	err = json.Unmarshal(body, &jobs)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return jobs, nil
 }
